a6681dfda11d5f2b4a46efb4c31daa5ec7d57206
NGASSAM NGAHAN Maxence Schnieder <ngassammaxence@gmail.com>
2024-10-15 14:32:08 +0100

    Update triangularisation.cpp

--- triangularisation.cpp
+++ triangularisation.cpp
@@ -14,16 +14,16 @@ int main(){
     std::cin>>n;
     
     for(i=1;i<=n;i++){
-        std::cout<<"Entrez la coordonnée en x "<<i ;
+        std::cout<<"Entrez la coordonnée en x \t "<<i ;
         std::cin>>x;
         sum_x = sum_x+ x;
     }
     for(i=1;i<=n;i++){
-        std::cout<<"Entrez la coordonnée en y"<<i ;
+        std::cout<<"Entrez la coordonnée en y \t"<<i ;
         std::cin>>y;
         sum_y = sum_y+ y;
     }
     xx = sum_x/n;
     yy = sum_y/n;
-    std::cout<<"le lieu a localiser est probablement au point de coordonnées ("<<xx<<";"<<yy<<")";
+    std::cout<<"le lieu a localiser est probablement au point de coordonnées ("<<xx<<";"<<yy<<")\n";
 }
